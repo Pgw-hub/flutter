084e4217a620982ac7753b32d2d370af2f33e4d8
parkgeonwoo <80042706+Pgw-hub@users.noreply.github.com>
2021-12-28 20:44:36 +0900

    scaffold practice

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,30 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     
     return MaterialApp(
-    home: Image.asset('assets/dog.jpeg')
-    
+      home : Scaffold(
+        appBar: AppBar(
+          title: Text('앱임')
+        ),
+        body: Column(
+          children: const [
+            Text('안녕')            
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          
+          child : SizedBox(
+            height : 70,
+            child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(Icons.phone),
+              Icon(Icons.message),
+              Icon(Icons.contact_page)
+            ],        
+        ),
+          )
+        ) 
+      )
     );
   }
 }
