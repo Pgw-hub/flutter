5e1cd08dc6cb3c6795127b398737543056159d27
parkgeonwoo <80042706+Pgw-hub@users.noreply.github.com>
2021-12-29 15:27:53 +0900

    구조틀

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home : Scaffold(
         appBar: AppBar(
-          leading : Icon(Icons.star),
+          leading : Icon(Icons.arrow_left_outlined),
           title: Text(//상단 바 이름 설정.
-            '앱임',
+            '키워드 관리',
             style: TextStyle(
               fontWeight: FontWeight.bold
               ),
@@ -23,13 +23,60 @@ class MyApp extends StatelessWidget {
           ),
           actions : [Icon(Icons.backspace_rounded)] // 위 오른쪽설정.
         ),
-        body: Row(
+        body:
+         Column(
+           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Text('안녕',
-             style: TextStyle( color : Colors.red) ),
-            ElevatedButton(onPressed: (){}, child: Text('버튼'))
+            Column(
+              children: [
+                Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text('내 키워드'),
+                    Text('비우기')
+                  ],  
+                ),
+                Text('키워드 정렬')
+              ],
+            ),
+            Column(
+              children: [
+                    Container(
+                      height : 30,
+                      color : Colors.blue,
+                      child : Row(
+                      children: [
+                        Text('추가하고 싶은 키워드 검색'),
+                        Icon(Icons.security_update_good_rounded)
+                      ],
+                    )
+                  ),
+              ],
+            ),
           ],
-        )
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child : SizedBox(
+             height : 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Icon(Icons.message),
+                    Text('전체공지'),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(Icons.star_border_outlined),
+                    Text('중요공지'),
+                  ],
+                ),
+                
+              ],        
+            ),
+          )
+        ) 
       )
     );
   }
